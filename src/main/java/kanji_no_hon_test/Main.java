84c5979d581d1edee78b0de6514c58aa312ef911
asdf913 <asdf913@gmail.com>
2022-08-29 23:46:52 +0900

    Add "Number Start" and "Number End" text field(s) Add "number" field in "Text" class Apply "java.awt.event.ActionListener" and "javax.swing.JFrame"

--- src/main/java/kanji_no_hon_test/Main.java
+++ src/main/java/kanji_no_hon_test/Main.java
@@ -1,5 +1,7 @@
 package kanji_no_hon_test;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -10,7 +12,6 @@ import java.lang.reflect.Member;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -20,9 +21,16 @@ import java.util.Optional;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
+import javax.swing.AbstractButton;
+import javax.swing.JButton;
 import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.text.JTextComponent;
 
 import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.function.FailableFunction;
 import org.apache.commons.lang3.reflect.FieldUtils;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
@@ -39,12 +47,248 @@ import freemarker.cache.ClassTemplateLoader;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
+import net.miginfocom.swing.MigLayout;
 
-public class Main {
+public class Main extends JFrame implements ActionListener {
+
+	private static final long serialVersionUID = -7531663604832571859L;
+
+	private static final String WRAP = "wrap";
+
+	private AbstractButton btnExecute = null;
+
+	private JTextComponent tfNumberStart, tfNumberEnd = null;
+
+	private Main() {
+	}
+
+	private void init() {
+		//
+		add(new JLabel("Number Range"));
+		//
+		add(tfNumberStart = new JTextField(), String.format("wmin %1$spx", 50));
+		//
+		add(new JLabel(" - "));
+		//
+		add(tfNumberEnd = new JTextField(), String.format("%1$s,wmin %2$spx", WRAP, 50));
+		//
+		add(new JLabel());
+		//
+		add(btnExecute = new JButton("Execute"), String.format("span %1$s", 3));
+		//
+		btnExecute.addActionListener(this);
+		//
+	}
+
+	@Override
+	public void actionPerformed(final ActionEvent evt) {
+		//
+		if (Objects.equals(evt != null ? evt.getSource() : null, btnExecute)) {
+			//
+			final JFileChooser jfc = new JFileChooser(".");
+			//
+			File file = null;
+			//
+			if (jfc.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+				//
+				file = jfc.getSelectedFile();
+				//
+			} // if
+				//
+			List<Text> texts = null;
+			//
+			try (final Workbook workbook = new XSSFWorkbook(file)) {
+				//
+				Text text = null;
+				//
+				int columnIndex;
+				//
+				boolean first = true;
+				//
+				Field[] fs = null;
+				//
+				Field f = null;
+				//
+				String string = null;
+				//
+				Integer integer = null;
+				//
+				IntMap<Field> intMap = null;
+				//
+				final Integer numberStart = valueOf(getText(tfNumberStart));
+				//
+				final Integer numberEnd = valueOf(getText(tfNumberEnd));
+				//
+				for (final Sheet sheet : workbook) {
+					//
+					if (sheet == null || sheet.iterator() == null) {
+						//
+						continue;
+						//
+					} // if
+						//
+					first = true;
+					//
+					for (final Row row : sheet) {
+						//
+						text = new Text();
+						//
+						if (row == null || row.iterator() == null) {
+							continue;
+						} // if
+							//
+						for (final Cell cell : row) {
+							//
+							if (first) {
+								//
+								if (fs == null) {
+									//
+									fs = FieldUtils.getAllFields(Text.class);
+									//
+								} // if
+									//
+								if ((intMap = ObjectUtils.getIfNull(intMap,
+										() -> Reflection.newProxy(IntMap.class, new IH()))) != null) {
+									//
+									intMap.setObject(cell.getColumnIndex(),
+											orElse(findFirst(testAndApply(Objects::nonNull, fs, Arrays::stream, null)
+													.filter(field -> Objects.equals(getName(field),
+															cell.getStringCellValue()))),
+													null));
+									//
+								} // if
+									//
+							} else if (intMap != null && intMap.containsKey(columnIndex = cell.getColumnIndex())
+									&& (f = intMap.getObject(columnIndex)) != null) {
+								// //
+								f.setAccessible(true);
+								//
+								if (Objects.equals(f.getType(), String.class)) {
+									//
+									if (Objects.equals(cell.getCellType(), CellType.NUMERIC)) {
+										//
+										string = Double.toString(cell.getNumericCellValue());
+										//
+									} else {
+										//
+										string = cell.getStringCellValue();
+										//
+									} // if
+										//
+									f.set(text = ObjectUtils.getIfNull(text, Text::new), string);
+									//
+								} else if (Objects.equals(f.getType(), Integer.class)) {
+									//
+									if (Objects.equals(cell.getCellType(), CellType.NUMERIC)) {
+										//
+										integer = Integer
+												.valueOf(Double.valueOf(cell.getNumericCellValue()).intValue());
+										//
+									} else {
+										//
+										integer = valueOf(cell.getStringCellValue());
+										//
+									} // if
+										//
+									f.set(text = ObjectUtils.getIfNull(text, Text::new), integer);
+									//
+								} // if
+									//
+							} // if
+								//
+						} // for
+							//
+						if (first) {
+							//
+							first = false;
+							//
+						} else if ((texts = ObjectUtils.getIfNull(texts, ArrayList::new)) != null) {
+							//
+							if (numberStart != null && text.number != null
+									&& numberStart.intValue() > text.number.intValue()) {
+								//
+								continue;
+								//
+							} else if (numberEnd != null && text.number != null
+									&& numberEnd.intValue() < text.number.intValue()) {
+								//
+								continue;
+								//
+							} //
+								//
+							texts.add(text);
+							//
+						} // if
+							//
+					} // for
+						//
+				} // for
+					//
+			} catch (final InvalidFormatException | IOException | IllegalAccessException e) {
+				//
+				// TODO Auto-generated catch block
+				//
+				e.printStackTrace();
+				//
+			} // try
+				//
+			final Configuration configuration = new Configuration(Configuration.VERSION_2_3_31);
+			//
+			configuration.setTemplateLoader(new ClassTemplateLoader(Main.class, "/"));
+			//
+			Template template = null;
+			//
+			try {
+				//
+				template = configuration.getTemplate("template.html.ftl");
+				//
+			} catch (final IOException e1) {
+				//
+				// TODO Auto-generated catch block
+				//
+				e1.printStackTrace();
+				//
+			} // try
+				//
+			if (template != null) {
+				//
+				try (final Writer w = new FileWriter(new File("test.html"))) {// TODO
+					//
+					final Map<String, Object> data = new LinkedHashMap<>(Collections.singletonMap("texts", texts));
+					//
+					template.process(data, w);
+					//
+				} catch (final IOException | TemplateException e) {
+					//
+					// TODO Auto-generated catch block
+					//
+					e.printStackTrace();
+					//
+				} // try
+					//
+			} // if
+				//
+		} // if
+			//
+	}
+
+	private static String getText(final JTextComponent instance) {
+		return instance != null ? instance.getText() : null;
+	}
+
+	private static Integer valueOf(final String instance) {
+		try {
+			return StringUtils.isNotBlank(instance) ? Integer.valueOf(instance) : null;
+		} catch (final NumberFormatException e) {
+			return null;
+		}
+	}
 
 	public static class Text {
 
 		private String text, hiragana = null;
+
+		private Integer number = null;
 
 		public String getText() {
 			return text;
@@ -54,132 +298,26 @@ public class Main {
 			return hiragana;
 		}
 
+		private Integer getNumber() {
+			return number;
+		}
+
 	}
 
-	public static void main(final String[] args)
-			throws InvalidFormatException, IOException, TemplateException, IllegalAccessException {
+	public static void main(final String[] args) {
 		//
-		final JFileChooser jfc = new JFileChooser(".");
+		final Main instance = new Main();
 		//
-		File file = null;
+		instance.setLayout(new MigLayout("debug"));// TODO
 		//
-		if (jfc.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
-			//
-			file = jfc.getSelectedFile();
-			//
-		} // if
-			//
-		List<Text> texts = null;
+		instance.init();
 		//
-		try (final Workbook workbook = new XSSFWorkbook(file)) {
-			//
-			Text text = null;
-			//
-			int columnIndex;
-			//
-			boolean first = true;
-			//
-			Field[] fs = null;
-			//
-			Field f = null;
-			//
-			String string = null;
-			//
-			IntMap<Field> intMap = null;
-			//
-			for (final Sheet sheet : workbook) {
-				//
-				if (sheet == null || sheet.iterator() == null) {
-					continue;
-				} // if
-					//
-				for (final Row row : sheet) {
-					//
-					text = new Text();
-					//
-					if (row == null || row.iterator() == null) {
-						continue;
-					} // if
-						//
-					for (final Cell cell : row) {
-						//
-						if (first) {
-							//
-							if (fs == null) {
-								//
-								fs = FieldUtils.getAllFields(Text.class);
-								//
-							} // if
-								//
-							if ((intMap = ObjectUtils.getIfNull(intMap,
-									() -> Reflection.newProxy(IntMap.class, new IH()))) != null) {
-								//
-								intMap.setObject(cell.getColumnIndex(), orElse(
-										findFirst(testAndApply(Objects::nonNull, fs, Arrays::stream, null).filter(
-												field -> Objects.equals(getName(field), cell.getStringCellValue()))),
-										null));
-								//
-							} // if
-								//
-						} else if (intMap != null && intMap.containsKey(columnIndex = cell.getColumnIndex())
-								&& (f = intMap.getObject(columnIndex)) != null) {
-							// //
-							f.setAccessible(true);
-							//
-							if (Objects.equals(f.getType(), String.class)) {
-								//
-								if (Objects.equals(cell.getCellType(), CellType.NUMERIC)) {
-									//
-									string = Double.toString(cell.getNumericCellValue());
-									//
-								} else {
-									//
-									string = cell.getStringCellValue();
-									//
-								} // if
-									//
-								f.set(text = ObjectUtils.getIfNull(text, Text::new), string);
-								//
-							}
-							//
-						} // if
-							//
-					} // for
-						//
-					if (first) {
-						//
-						first = false;
-						//
-					} else if ((texts = ObjectUtils.getIfNull(texts, ArrayList::new)) != null) {
-						//
-						texts.add(text);
-						//
-					} // if
-						//
-				} // for
-					//
-			} // for
-				//
-		} // try
-			//
-		final Configuration configuration = new Configuration(Configuration.VERSION_2_3_31);
+		instance.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		//
-		configuration.setTemplateLoader(new ClassTemplateLoader(Main.class, "/"));
+		instance.pack();
 		//
-		final Template template = configuration.getTemplate("template.html.ftl");
+		instance.setVisible(true);
 		//
-		if (template != null) {
-			//
-			try (final Writer w = new FileWriter(new File("test.html"))) {// TODO
-				//
-				final Map<String, Object> data = new LinkedHashMap<>(Collections.singletonMap("texts", texts));
-				//
-				template.process(data, w);
-				//
-			} // try
-				//
-		} // if
-			//
 	}
 
 	private static class IH implements InvocationHandler {
@@ -276,12 +414,6 @@ public class Main {
 
 	private static String getName(final Member instance) {
 		return instance != null ? instance.getName() : null;
-	}
-
-	private static <E> void add(final Collection<E> items, final E item) {
-		if (items != null) {
-			items.add(item);
-		}
 	}
 
 }
